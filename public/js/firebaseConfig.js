478fac596ab478baedaae3b7ea386b74c8303052
Siri Helene Wahl <157708887+sirihelenew@users.noreply.github.com>
2024-10-13 22:07:26 +0200

    Update firebaseConfig.js

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -6,12 +6,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBxy0QD8ORm-kHd_U66v0uhbdy1TnKEwV4",
-    authDomain: "sirihelenewahl.firebaseapp.com",
-    projectId: "sirihelenewahl",
-    storageBucket: "sirihelenewahl.appspot.com",
-    messagingSenderId: "347857962806",
-    appId: "1:347857962806:web:86fb0ee7948b739ca81597"
+    
 };
 
 // Initialize Firebase
